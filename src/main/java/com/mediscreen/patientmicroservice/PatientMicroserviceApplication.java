8b3d36663ef42c01cbbc9340dc436d83427f033a
Subhi YD <subhy809fr@gmail.com>
2023-05-09 22:01:32 +0200

    v1.5.2 update application.properties, Controller

--- src/main/java/com/mediscreen/patientmicroservice/PatientMicroserviceApplication.java
+++ src/main/java/com/mediscreen/patientmicroservice/PatientMicroserviceApplication.java
@@ -2,12 +2,10 @@ package com.mediscreen.patientmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class PatientMicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PatientMicroserviceApplication.class, args);
 	}
-
 }
--- src/main/java/com/mediscreen/patientmicroservice/controller/PatientController.java
+++ src/main/java/com/mediscreen/patientmicroservice/controller/PatientController.java
@@ -7,6 +7,7 @@ import com.mediscreen.patientmicroservice.service.PatientService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -24,6 +25,8 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/api")
+@Transactional
+@CrossOrigin(origins = "http://localhost:4200")
 public class PatientController {
     private static final Logger logger = LoggerFactory.getLogger(PatientController.class);
 
